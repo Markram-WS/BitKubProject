951b1248d338c37758614d7b2e7d1bd3faea9314
Mark <mark_AI01@hotmail.com>
2020-09-23 07:32:21 +0700

    plot garph

--- gendata.csv.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient,results 
-_client=MongoClient("mongodb+srv://wasan:1234@cluster0.ujivx.gcp.mongodb.net/trading_db?retryWrites=true&w=majority")
-_db=_client.get_database('trading_db')   
-_db['bitkub_trade'].find({}):
